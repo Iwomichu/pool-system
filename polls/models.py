848f6ce6d1bcbc7406c793ef31171cd00495b604
Jaro197 <kraus_197@onet.eu>
2020-01-11 21:54:22 +0100

    update models.py

--- polls/models.py
+++ polls/models.py
@@ -5,16 +5,16 @@ from django.contrib.auth.models import User
 class Poll(models.Model):
     title = models.CharField(max_length=100)
     created = models.DateTimeField(auto_now_add=True)
-    owner = models.ForeignKey(User, related_name="polls" on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, related_name="polls", on_delete=models.CASCADE)
 
 class PollOption(models.Model):
     text = models.CharField(max_length=100)
     poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
     
 class Vote(models.Model):
-    user = models.ForeignKey(User, related_name="votes" on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, related_name="votes", on_delete=models.CASCADE, null=True)
     poll_option = models.ForeignKey(PollOption, on_delete=models.CASCADE)
     ip_address = models.GenericIPAddressField()
 
     class Meta:
-        unique_together = [("user", "poll_option"),("ip_address", "poll_option")]]
+        unique_together = (("user", "poll_option"),("ip_address", "poll_option"),)
