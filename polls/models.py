aea2569cd4af79950bf0bbf1aff51e7437f34575
Jaro197 <kraus_197@onet.eu>
2020-01-11 19:50:55 +0100

    Update settings.py and create models in poll app

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Poll(models.Model):
+    title = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, related_name="polls" on_delete=models.CASCADE)
+
+class PollOption(models.Model):
+    text = models.CharField(max_length=100)
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    
+class Vote(models.Model):
+    user = models.ForeignKey(User, related_name="votes" on_delete=models.CASCADE, null=True)
+    poll_option = models.ForeignKey(PollOption, on_delete=models.CASCADE)
+    ip_address = models.GenericIPAddressField()
+
+    class Meta:
+        unique_together = [("user", "poll_option"),("ip_address", "poll_option")]]
