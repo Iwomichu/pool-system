ee3e5ce0dfe37ba674b0d41461fa5d64b847bbc9
Jaro197 <kraus_197@onet.eu>
2020-02-03 19:20:18 +0100

    Added djoser.

--- poolsystem/urls.py
+++ poolsystem/urls.py
@@ -15,12 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('frontend.urls')),
     path('auth/', include('accounts.urls')),
-    path('api/', include('polls.api.urls')),
-    path('checkserver/', index)
+    path('api/', include('polls.api.urls'))
 ]
